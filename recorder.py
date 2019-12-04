9473e877a44388551316abecf6e972d500ac8e87
lufy90 <very90@foxmail.com>
2019-12-04 23:39:13 +0800

    formating any operation on new branch format.

--- recorder.py
+++ recorder.py
@@ -6,6 +6,7 @@ import json
 
 
 '''Any operation should be indicated as a tuple in [(),(),...]'''
+'''Turn all Operation Objects to string and save in files, '''
 class Recorder:
     '''class Recorder, '''
 
@@ -14,24 +15,35 @@ class Recorder:
 
 class Operation:
     ''' nnnn  '''
-    def record(self):
+    key_op = keyboard.Controller()
+    mouse_op = mouse.Controller()
+    op = {  'pressm': mouse_op.press,
+            'releasem': mouse_op.release,
+            'click': mouse_op.click,
+            'scroll': mouse_op.scroll,
+            'move': mouse_op.move,
+            'position': mouse_op.position,
+            'pressk': key_op.press,
+            'releasek': key_op.release,
+            }
+
+#    def record(self):
+    def __init__(self):
         '''return op object: 2 elements tuple: (action, arguments)'''
         out=[]
-        key_op = keyboard.Controller()
-        mouse_op = mouse.Controller()
         def on_press(key):
-            out.append((key_op.press, key))
+            out.append(('pressk', format(key)))
         def on_release(key):
             if key == keyboard.Key.esc:
                 mouse_listener.stop()
                 keyboard_listener.stop()
-            out.append((key_op.release, key))
+            out.append(('releasek', key))
         def on_move(x,y): 
-            out.append((mouse_op.move, (x,y)))
+            out.append(('move', (x,y)))
         def on_click(x,y,button,pressed):
-            out.append((mouse_op.click, button))
+            out.append(('click', button))
         def on_scroll(x,y,dx,dy):
-            out.append((mouse_op.scroll,(dx,dy)))
+            out.append(('scroll',(dx,dy)))
 
         keyboard_listener = keyboard.Listener(on_press=on_press,
                 on_release=on_release)
@@ -41,11 +53,16 @@ class Operation:
         keyboard_listener.start()
         mouse_listener.start()
 
-        return out
+        self.out = out
 
     def record_to_json(self, filename='test.json'):
+        '''NOT ready for now'''
         with open(filename, 'w') as f:
-            json.dump(self.record(), f)
+            json.dump(self.out, f)
+
+    def record_to_pickel(self, filename='test.pkl'):
+        with open(filename, 'w') as f:
+            pickel.dump(self.out, f)
 
 
     def repeat(self, op):
